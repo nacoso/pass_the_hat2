91fb7d38faa49d21b90f8d2129f0fbf464c69d2a
Alex Clivio <amrclivio@gmail.com>
2018-02-05 14:22:37 -0500

    created controller and views, added stripe subscription, added keys, added .env

---
+++ app/controllers/charges_controller.rb
@@ -0,0 +1,27 @@
+class ChargesController < ApplicationController
+
+def new
+end
+
+def create
+# Amount in cents
+# @amount = 500
+
+  customer = Stripe::Customer.create(
+    :email => params[:stripeEmail],
+    :source  => params[:stripeToken]
+  )
+  plan = Stripe::Plan.retrieve("#{params[:plan]}")
+  subscription = Stripe::Subscription.create({
+     customer: customer.id,
+     plan: plan
+  })
+
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_charge_path
+  end
+
+  # user = User.find(params[:user_id])
+  # redirect_to user
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :user_charities
   resources :charities
   resources :users
+  resources :charges
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
