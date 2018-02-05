342bb852212705e1be1b0109b507f2118a302e9a
Nick Cardoso <nac.doso@gmail.com>
2018-02-05 15:07:51 -0500

    rails g controller sessions with new create and destroy actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'session/new'
+
+  get 'session/destroy'
+
+  get 'session/create'
+
   get 'sessions/new'
   get 'sessions/create'
   get 'sessions/destroy'
